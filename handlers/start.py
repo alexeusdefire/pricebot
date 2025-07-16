e7c37363153877482c9429da72b9037ac6c39159
alexeusdefire <alexeusdefire@duck.com>
2025-07-16 17:09:18 +0300

    edited output text

--- handlers/start.py
+++ handlers/start.py
@@ -6,4 +6,4 @@ router = Router()
 @router.message(CommandStart())
 async def process_start(message: Message):
     await message.answer(text="type token ticker to see current price on Binance, "
-                              "ByBit, MEXC, GateIo, BingX")
+                              "ByBit, MEXC, GateIo, BingX, OKX")
