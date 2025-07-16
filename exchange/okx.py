a7d216793dc924aea25882e8a1b75b5b10b93cc4
alexeusdefire <alexeusdefire@duck.com>
2025-07-16 16:13:27 +0300

    fixed OKX module

--- exchange/okx.py
+++ exchange/okx.py
@@ -4,7 +4,7 @@ from .exchange import BaseExchange
 class OKXExchange(BaseExchange):
     def __init__(self):
         super().__init__(
-            base_url="https://www.okx.com/api/v5/market",
+            base_url="https://www.okx.com/api/v5/market/ticker?instId=",
             name="OKX"
         )
 
@@ -14,13 +14,14 @@ class OKXExchange(BaseExchange):
     async def get_price(self, ticker):
         try:
             formatted_ticker = self.format_ticker(ticker)
-            url = f"{self.base_url}/ticker?instId={formatted_ticker}"
+            url = f"{self.base_url}{formatted_ticker}"
 
             async with self.session.get(url) as response:
                 if response.status == 200:
                     data = await response.json()
-                    if data["code"] == 0:
+                    if data["code"] == "0":
                         return data["data"][0]["last"]
+
                     else:
                         return "no token"
                 else:
